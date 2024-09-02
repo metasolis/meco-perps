1a23a6debae48414edd9ff77296f499c516adb34
witwiki <Vikram.Udyawer@community.isunet.edu>
2024-09-02 15:56:10 +1200

    deployed to devnet but still net to upload idl

--- programs/perpetuals/src/lib.rs
+++ programs/perpetuals/src/lib.rs
@@ -24,7 +24,7 @@ solana_security_txt::security_txt! {
     auditors: "Halborn"
 }
 
-declare_id!("Bmr31xzZYYVUdoHmAJL1DAp2anaitW8Tw9YfASS94MKJ");
+declare_id!("3LjnwXNbvUe9DB2qaTNvCL4sVvtvshCEeydhzUfkbJTB");
 
 #[program]
 pub mod perpetuals {
